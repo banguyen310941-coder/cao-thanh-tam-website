b975278f7f0171730cd7d9c865f0a0dfa1646949
banguyen310941-coder <banguyen310941@gmail.com>
2026-08-31 06:50:54 +0700

    Load readability and location refinements

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import "./admin-enhancements.css";
 import "./home-polish.css";
 import "./home-fixes.css";
 import "./home-reference.css";
+import "./readability-fixes.css";
 
 const display = Cormorant_Garamond({ subsets: ["latin", "vietnamese"], variable: "--font-display", weight: ["500", "600", "700"] });
 const sans = Be_Vietnam_Pro({ subsets: ["latin", "vietnamese"], variable: "--font-sans", weight: ["300", "400", "500", "600", "700"] });
