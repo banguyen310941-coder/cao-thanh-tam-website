83b8ccca927abfc416012110676c1e080b99a9a0
banguyen310941-coder <banguyen310941@gmail.com>
2026-08-31 06:37:26 +0700

    Load homepage fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import "./homepage.css";
 import "./cms.css";
 import "./admin-enhancements.css";
 import "./home-polish.css";
+import "./home-fixes.css";
 
 const display = Cormorant_Garamond({ subsets: ["latin", "vietnamese"], variable: "--font-display", weight: ["500", "600", "700"] });
 const sans = Be_Vietnam_Pro({ subsets: ["latin", "vietnamese"], variable: "--font-sans", weight: ["300", "400", "500", "600", "700"] });
